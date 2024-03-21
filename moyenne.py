c27693be4ee6ee245a436abe3eb9e6ba40ae9fbf
Marinounouh <m.kazemi@H3hitema.fr>
2024-03-21 10:49:40 +0100

    moyenne

--- moyenne.py
+++ moyenne.py
@@ -4,9 +4,13 @@ def calculer_moyenne(liste):
     return sum(liste) / len(liste)
 
 def test_calculer_moyenne():
+    print("Test 1: ", calculer_moyenne([1, 2, 3, 4, 5]))
     assert calculer_moyenne([1, 2, 3, 4, 5]) == 3
+    print("Test 2: ", calculer_moyenne([]))
     assert calculer_moyenne([]) == 0
+    print("Test 3: ", calculer_moyenne([0, 0, 0, 0]))
     assert calculer_moyenne([0, 0, 0, 0]) == 0
+    print("Test 4: ", calculer_moyenne([-1, 1]))
     assert calculer_moyenne([-1, 1]) == 0
 
 if __name__ == "__main__":
